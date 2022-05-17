beeb40f9610de94e2e6fdbfd75031af80ebd4a96
Ubuntu <ubuntu@ip-172-31-38-180.us-west-2.compute.internal>
2022-05-17 14:46:48 +0000

    so far so good, the .tf file is ready to set- up infrastructure, just missing to configure the deployment of the app

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,4 +1,12 @@
+locals {
+  subnet1  = "subnet-0691fe57c087e2991"
+  subnet2  = "subnet-0e958c727c69097a4"
+}
+
+
+
 # Creating a policy
+
 
 resource "aws_iam_role" "eks_cluster" {
   name = "eks-cluster"
@@ -44,7 +52,7 @@ resource "aws_eks_cluster" "aws_eks" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids              = ["subnet-0691fe57c087e2991", "subnet-0e958c727c69097a4"]
+    subnet_ids              = [local.subnet1]
     endpoint_private_access = true
     endpoint_public_access  = true
   }
@@ -54,3 +62,66 @@ resource "aws_eks_cluster" "aws_eks" {
   }
 }
 
+
+
+
+resource "aws_iam_role" "eks_nodes" {
+  name = "final_project_node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+
+
+
+
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.aws_eks.name
+  node_group_name = "Node_group"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = [local.subnet1]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 2
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+
+
+}

786a0778485ada38702da8f5c2cfc64003719407
Ubuntu <ubuntu@ip-172-31-38-180.us-west-2.compute.internal>
2022-05-17 12:25:51 +0000

    Expanded setup.sh, including installation of awscli

--- setup.sh
+++ setup.sh
@@ -25,3 +25,6 @@ sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl 
 
+# Installing CLI
+sudo apt install awscli
+

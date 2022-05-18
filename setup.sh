206631266ad9112682d47c13d0a437df154d2465
Ubuntu <ubuntu@ip-172-31-38-180.us-west-2.compute.internal>
2022-05-18 13:15:19 +0000

    The app builds fine, needs to be deployed

--- setup.sh
+++ setup.sh
@@ -7,6 +7,20 @@ unzip terraform_*_linux_*.zip
 sudo mv terraform /usr/local/bin
 rm terraform_*_linux_*.zip
 
+
+# Install Docker
+sudo apt-get update
+sudo apt install curl -y
+curl https://get.docker.com | sudo bash
+
+# Install Docker-compose
+udo apt update
+sudo apt install -y jq
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
 # Installing Ansible  -  with pip3
 sudo apt install python3-pip -y
 mkdir -p ~/.local/bin

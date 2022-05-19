e68901e7896033d4eedd1e64047c03aa0ae9be98
Ubuntu <ubuntu@ip-172-31-38-180.us-west-2.compute.internal>
2022-05-19 10:42:08 +0000

    reduced the setup.sh file as we Ansible is not required

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ sudo apt-get update
 sudo apt install curl -y
 curl https://get.docker.com | sudo bash
 
+
 # Install Docker-compose
 udo apt update
 sudo apt install -y jq
@@ -21,23 +22,17 @@ sudo curl -L "https://github.com/docker/compose/releases/download/${version}/doc
 sudo chmod +x /usr/local/bin/docker-compose
 
 
-# Installing Ansible  -  with pip3
-sudo apt install python3-pip -y
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-pip3 install --user ansible
-ansible --version
-
 # Installing eksctl
 sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"
 sudo mv /tmp/eksctl /usr/local/bin
+
 
 # Setting cluster API 
 echo "Setting up cluster API through kubectl"
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl 
+
 
 # Installing CLI
 sudo apt install awscli
